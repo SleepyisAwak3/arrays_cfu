5f60c6ad29ad16ebf927a46ecbe2282892b322ca
Noah Byers <noahxbyers@gmail.com>
2022-11-09 18:44:32 -0800

    Add Methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,11 @@
+// "".length" is a Javascript method that runs through all the elements in an array.
 var names = ["John", "V", "Judy", "Panamn"]
-var numbers = [12, 4, 20, 51]
-var boos = [true, false, true, false]
+ console.log(names.length())
+
+// ".pop()" Method is when the console access the last number, gets rid of it, and then returns it as the output we see.
+ var numbers = [12, 4, 20, 51]
+  console.log(numbers.pop(4))
+
+// ".reverse()" Method prints out the array we have, but in the reverse order.
+  var boos = [true, false, true, false]
+  console.log(boos.reverse())
