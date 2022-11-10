01d2b44f9abaef19970c1fb0a72caf30d17e8e75
Noah Byers <noahxbyers@gmail.com>
2022-11-09 18:50:27 -0800

    Add Index Positioning

--- arrays.js
+++ arrays.js
@@ -1,6 +1,7 @@
-// "".length" is a Javascript method that runs through all the elements in an array.
+// "".length" is a Javascript method that runs through all the elements in an array. However in this line,
+// it only prints out the amount of elements in the array when ran.
 var names = ["John", "V", "Judy", "Panamn"]
- console.log(names.length())
+ console.log(names.length)
 
 // ".pop()" Method is when the console access the last number, gets rid of it, and then returns it as the output we see.
  var numbers = [12, 4, 20, 51]
@@ -9,3 +10,14 @@ var names = ["John", "V", "Judy", "Panamn"]
 // ".reverse()" Method prints out the array we have, but in the reverse order.
   var boos = [true, false, true, false]
   console.log(boos.reverse())
+
+// The index file position is just of that as counting with a number line. 
+// It starts with 0 as it does infact have value, and then counts up from there. So regularly it would go 1,2,3,4; however
+// it goes 0,1,2,3... 
+
+var example = [4, 5, 2, 8]
+ console.log(example[3])
+ console.log(example[0])
+ console.log(example[2])
+// With in the example code above you can see that the pink square brackets are going to print out the numbers 8,4, and 2.
+// As those are where the elements lie in the index positioning.
